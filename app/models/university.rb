4c1717dff650e2a087acf9c93dbe617e58972e36
DonaNobisPacem <johnthomasraphael@yahoo.com>
2015-05-04 18:47:26 +0900

    Updated University views + seeds

--- app/models/university.rb
+++ app/models/university.rb
@@ -2,5 +2,5 @@ class University < ActiveRecord::Base
 	validates :univ_name, presence: true, uniqueness: true
 	has_many :university_profiles, :dependent => :destroy
 	accepts_nested_attributes_for :university_profiles, :allow_destroy => true
-	#mount_uploader :image, ImageUploader
+	mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430090413) do
+ActiveRecord::Schema.define(version: 20150504044005) do
 
   create_table "static_pages", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150430090413) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.string   "univ_name",  limit: 255
+    t.string   "image",      limit: 255
   end
 
   create_table "university_profiles", force: :cascade do |t|
